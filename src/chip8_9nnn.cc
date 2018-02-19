03368039d9b18b9ac3b21d7ae3cb0aca7da15d9f
roitk <rohit.kothur@gmail.com>
2018-02-19 13:00:52 -0700

    Implement 9NNN

--- src/chip8_9nnn.cc
+++ src/chip8_9nnn.cc
@@ -2,10 +2,18 @@
 namespace chip8 {
 
 /*
- *
+ * 9XY0: Skip next instruction if VX != VY
  */
 void Emulator::Op9NNN(unsigned short opcode) {
-    
+    if(opcode_ & 0x000F) {
+        // Unknown opcode, last nibble should be 0
+    }
+    int x = opcode_ & 0x0F00 >> 8;
+    int y = opcode_ & 0x00F0 >> 4;
+    if(registers_[x] == registers_[y]) {
+        pc_ += 2;
+    }
+    pc_ += 2;
 }
 
 }
